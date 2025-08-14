34b788f53145459edc7c8f030ca761f5b99af399
Prof. Alessandro <alessandro.vitorio@etec.sp.gov.br>
2025-08-14 01:26:03 +0000

    aula-130825

---
+++ lista-1/exercicio1.js
@@ -0,0 +1,27 @@
+let input =  require('readline-sync');
+
+// trapesio entrda
+let n1 =  parseInt( input.question("Digite um numero: "))
+
+// retangulo processamento
+// n1 = n1 + 2
+
+// //exibir - saida
+// console.log("o numero digitado é: ",  n1);
+
+// // retangulo processamento
+// n1 = n1 -1
+// //exibir - saida
+// console.log("o numero digitado é: " , n1);
+
+// // retangulo processamento
+// n1 = n1 * 5
+// //exibir - saida
+// console.log("o numero digitado é: "  + n1);
+
+// // retangulo processamento
+// n1 = n1 / 2
+// //exibir - saida
+// console.log("o numero digitado é: "  ,n1);
+
+
